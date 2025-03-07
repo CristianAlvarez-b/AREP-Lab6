a7e49e4e75b2208a32ae2ab14cb3c320e971a519
unknown <cristian.alvarez-b@labinfo.is.escuelaing.edu.co>
2025-03-07 07:43:09 -0500

    Initial

---
+++ src/main/java/co/edu/escuelaing/edu/arep/secure/SecureApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.edu.arep.secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureApplication.class, args);
+	}
+
+}
